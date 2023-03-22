2caf44082660f6540b5ad982e9e06c6c07412b5a
Faraz <farazatgithub@gmail.com>
2023-03-22 10:31:03 +0500

    find square root

---
+++ Search Algos/binary-search/findSqrtRoot.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int findSquareRoot(int n)
+{
+    int start = 0;
+    int end = n;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (mid * mid == n)
+        {
+            return mid;
+        }
+        else if (mid * mid > n)
+        {
+            end = mid - 1;
+        }
+        else if (mid * mid < n)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    cout << "enter the num you want to calculate square root of: ";
+    int num;
+    cin >> num;
+
+    int result = findSquareRoot(num);
+    cout << "square root of " << num << " is :" << result << endl;
+}
