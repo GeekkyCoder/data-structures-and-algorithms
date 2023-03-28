2dd0f6c410cac220bbcf64dd59d1290332c52ac5
Faraz <farazatgithub@gmail.com>
2023-03-28 16:32:00 +0500

    temp var in insertion sort

--- Sort Algos/insertion-sort/insertion-sort.cpp
+++ Sort Algos/insertion-sort/insertion-sort.cpp
@@ -7,17 +7,17 @@ int main(){
     
     // insertion sort 
     for(int i=1; i<n;i++){
-        int key = a[i];
+        int temp = a[i];
         int j = i -1 ;
         while(j >=0){
-            if(a[j] > key){
+            if(a[j] > temp){
                 a[j+1] = a[j];
             }else {
                 break;
             }
             j--;
         }
-        a[j+1] = key;
+        a[j+1] = temp;
         }
         
         for(int p=0 ; p<n;p++){
